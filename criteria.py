159f5ea0cd135a28f4a97d7db1f561e9492cb043
jacktsmith2 <108944215+jacktsmith2@users.noreply.github.com>
2025-06-12 23:03:23 -0400

    Update criteria.py
    Added category

--- criteria.py
+++ criteria.py
@@ -2,181 +2,223 @@ CRITERIA = [
 {
     "label": "Season 1",
     "description": "Queen appeared in Season 1 (any franchise).",
-    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 1)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 1)",
+    "category": "season"
 },
 {
     "label": "Season 2",
     "description": "Queen appeared in Season 2 (any franchise).",
-    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 2)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 2)",
+    "category": "season"
 },
 {
     "label": "Season 3",
     "description": "Queen appeared in Season 3 (any franchise).",
-    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 3)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 3)",
+    "category": "season"
 },
 {
     "label": "Season 4",
     "description": "Queen appeared in Season 4 (any franchise).",
-    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 4)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 4)",
+    "category": "season"
 },
 {
     "label": "Season 5",
     "description": "Queen appeared in Season 5 (any franchise).",
-    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 5)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 5)",
+    "category": "season"
 },
 {
     "label": "Season 6",
     "description": "Queen appeared in Season 6 (any franchise).",
-    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 6)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 6)",
+    "category": "season"
 },
 {
     "label": "Season 7",
     "description": "Queen appeared in Season 7 (any franchise).",
-    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 7)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 7)",
+    "category": "season"
 },
 {
     "label": "Season 8",
     "description": "Queen appeared in Season 8 (any franchise).",
-    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 8)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 8)",
+    "category": "season"
 },
 {
     "label": "Season 9",
     "description": "Queen appeared in Season 9 (any franchise).",
-    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 9)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 9)",
+    "category": "season"
 },
 {
     "label": "Season 10",
     "description": "Queen appeared in Season 10 (any franchise).",
-    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 10)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 10)",
+    "category": "season"
 },
 {
     "label": "Season 11",
     "description": "Queen appeared in Season 11 (any franchise).",
-    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 11)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 11)",
+    "category": "season"
 },
 {
     "label": "Season 12",
     "description": "Queen appeared in Season 12 (any franchise).",
-    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 12)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 12)",
+    "category": "season"
 },
 {
     "label": "Season 13",
     "description": "Queen appeared in Season 13 (any franchise).",
-    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 13)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 13)",
+    "category": "season"
 },
 {
     "label": "Season 14",
     "description": "Queen appeared in Season 14 (any franchise).",
-    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 14)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 14)",
+    "category": "season"
 },
 {
     "label": "Season 15",
     "description": "Queen appeared in Season 15 (any franchise).",
-    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 15)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 15)",
+    "category": "season"
 },
 {
     "label": "Season 16",
     "description": "Queen appeared in Season 16 (any franchise).",
-    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 16)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 16)",
+    "category": "season"
+},
+{
+    "label": "Season 17",
+    "description": "Queen appeared in Season 17 (any franchise).",
+    "sql": "queen_id IN (SELECT queen_id FROM queens WHERE season_no = 16)",
+    "category": "season"
 },
 {
     "label": "Returning Queen",
     "description": "Queen returned to the competition during any season.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(is_returning) >= 1)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(is_returning) >= 1)",
+    "category" : "appearances"
 },
 {
     "label": "Multi-Season Queen",
     "description": "Queen appeared in more than one season.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING COUNT(DISTINCT season_no) > 1)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING COUNT(DISTINCT season_no) > 1)",
+    "category" : "appearances"
 },
 {
     "label": "No Maxi Wins",
     "description": "Queen has zero maxi challenge wins.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) = 0)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) = 0)",
+    "category" : "maxi_wins"
 },
 {
     "label": "1 Maxi Win",
     "description": "Queen has one maxi challenge win.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) = 1)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) = 1)",
+    "category" : "maxi_wins"
 },
 {
     "label": "2 Maxi Wins",
     "description": "Queen has two maxi challenge wins.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) = 2)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) = 2)",
+    "category" : "maxi_wins"
 },
 {
     "label": "3 Maxi Wins",
     "description": "Queen has three maxi challenge wins.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) = 3)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) = 3)",
+    "category" : "maxi_wins"
 },
 {
     "label": "4 Maxi Wins",
     "description": "Queen has four maxi challenge wins.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) = 4)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) = 4)",
+    "category" : "maxi_wins"
 },
 {
     "label": "1+ Maxi Win",
     "description": "Queen has at least one maxi challenge win.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) > 0)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) > 0)",
+    "category" : "at_least_maxi_wins"
 },
 {
     "label": "2+ Maxi Wins",
     "description": "Queen has at least two maxi challenge wins.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) >= 2)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) >= 2)",
+    "category" : "at_least_maxi_wins"
 },
 {
     "label": "3+ Maxi Wins",
     "description": "Queen has at least three maxi challenge wins.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) >= 3)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) >= 3)",
+    "category" : "at_least_maxi_wins"
 },
 {
     "label": "4+ Maxi Wins",
     "description": "Queen has at least four maxi challenge wins.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) >= 4)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(win) >= 4)",
+    "category" : "at_least_maxi_wins"
 },
 {
     "label": "No Lip Sync Wins",
     "description": "Queen has zero lip sync wins.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) = 0)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) = 0)",
+    "category" : "lipsync_wins"
 },
 {
     "label": "1 Lip Sync Win",
     "description": "Queen has one lip sync win.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) = 1)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) = 1)",
+    "category" : "lipsync_wins"
 },
 {
     "label": "2 Lip Sync Wins",
     "description": "Queen has two lip sync wins.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) = 2)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) = 2)",
+    "category" : "lipsync_wins"
 },
 {
     "label": "3 Lip Sync Wins",
     "description": "Queen has three lip sync wins.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) = 3)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) = 3)",
+    "category" : "lipsync_wins"
 },
 {
     "label": "4 Lip Sync Wins",
     "description": "Queen has four lip sync wins.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) = 4)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) = 4)",
+    "category" : "lipsync_wins"
 },
 {
     "label": "1+ Lip Sync Win",
     "description": "Queen has at least one lip sync win.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) >= 1)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) >= 1)",
+    "category" : "at_least_lipsync_wins"
 },
 {
     "label": "2+ Lip Sync Wins",
     "description": "Queen has at least two lip sync wins.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) >= 2)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) >= 2)",
+    "category" : "at_least_lipsync_wins"
 },
 {
     "label": "3+ Lip Sync Wins",
     "description": "Queen has at least three lip sync wins.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) >= 3)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) >= 3)",
+    "category" : "at_least_lipsync_wins"
 },
 {
     "label": "4+ Lip Sync Wins",
     "description": "Queen has at least four lip sync wins.",
-    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) >= 4)"
+    "sql": "queen_id IN (SELECT queen_id FROM queens GROUP BY queen_id HAVING SUM(lip_sync_wins) >= 4)",
+    "category" : "at_least_lipsync_wins"
 }
 ]
